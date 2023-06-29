985282611b3160c87a2c900c42f6e7b7f2dfa047
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-06-29 15:32:48 +0200

    temp state: change package descr., remove old content from server.js and upload.html add simple article structure to upload html

--- server.js
+++ server.js
@@ -3,67 +3,32 @@
 // VARIABLEN und KONSTANTEN
 let filenames = {};
 const paths = {
-    filelist: 'data/filelist.json'
+    filelist: 'data/articels.json'
 }
 
 // FORMULARKRAM
-import formidable from 'formidable';
 import fs from 'fs';
 
 // WEBSERVER
-// Express kann alternativ auch als ES-Modul importiert werden
 import express from 'express';
 
-// const express = require('express');
 const server = express();
 
 server.use(express.static('public', {
     extensions: ['html']
 }));
 
-// JSON-Daten aus einer POST-Anfrage eytrahieren und in das body-Attribut schreiben
+// JSON-Daten aus einer POST-Anfrage extrahieren und in das body-Attribut schreiben
 server.use(express.json());
 
 // ROUTEN
 server.post('/handle_form', (request, response) => {
-    // Formidable-Objekt anlegen
-    const form = formidable({
-        uploadDir: 'public/uploads',
-        keepExtensions: true
-    });
+    console.log(request.body);	
 
-    // Formulardaten verarbeiten und die Daten als Parameter an die Callback-Funktion geben
-    form.parse(request, (err, felder, dateien) => {
-        if (err) console.log(err);
-        else {
-            // console.log(felder);
-            // console.log(dateien);
-
-            // Alte und neue Dateinamen miteinander verknüpfen
-            // Die Dateien sind ein Array, das iteriert werden muss
-            dateien.upload.forEach(file => {
-                // Für jede Datei einen Eintrag im filenames-Objekt vornehmen
-                filenames[file.newFilename] = file.originalFilename;
-            })
-
-            response.json({
-                status: 'OK',
-                filenames
-            })
-
-        }
-    })
+    
 })
 
-const storeFilelist = () => {
-    fs.writeFile(
-        paths.filelist,
-        JSON.stringify(filenames),
-        err => {
-            if (err) console.log(err);
-        }
-    )
-}
+
 
 const init = () => {
     // Liste hochgeladener Dateien laden
