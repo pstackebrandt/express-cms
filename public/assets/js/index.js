97cc23e19470e6bcdf2954df2e7f6121b8f314c7
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-01 10:28:38 +0200

    Improved default text of  article Remove rendering of file list (code from template)

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -6,53 +6,21 @@ import dom from './dom.js';
 const elements = {};
 
 // FUNKTIONEN
-const domMapping = () => {
-    elements.myForm = dom.$('#formUpload');
-    elements.main = dom.$('main');
-}
-
-const renderFilelist = filelist => {
-    console.log(filelist);
-    elements.main.innerHTML = '';
-
-    // Objekt iterieren
-    Object.entries(filelist).forEach(file => {
-        const container = dom.create({
-            type: 'p',
-            classes: ['container'],
-            parent: elements.main
-        })
-
-        dom.create({
-            type: 'a',
-            parent: container,
-            attr: {
-                href: `uploads/${file[0]}`,
-                download: file[1]
-            },
-            content: file[1]
-        })
-    })
-}
-
 const sendData = evt => {
     evt.preventDefault();
 
-    // Daten aus dem Formuöar lesen und in die Variable schreiben
+    // Daten aus dem Formular lesen und in die Variable schreiben
     let payload = new FormData(elements.myForm);
 
     fetch('/handle_form', {
         method: 'post',
-        // headers werden nicht benötigt, da FormData standard ist
+        // Headers werden nicht benötigt, da FormData standard ist.
         body: payload
     }).then(
         res => res.json()
-    ).then(
-        res => renderFilelist(res.filenames)
     ).catch(
         console.warn
     )
-
 }
 
 const appendEventlisteners = () => {
@@ -60,7 +28,6 @@ const appendEventlisteners = () => {
 }
 
 const init = () => {
-    domMapping();
     appendEventlisteners();
 }
 
